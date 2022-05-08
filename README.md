b12b07cb10e2313da42f9daf38fc643132e3cc09
Manny <manroven-singh@hotmail.com>
2022-05-08 17:33:00 +0930

    adding code to generate markdown and rough draft of text to be used in readme

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Professional-ReadMe-Generator
+
+# Dummy Readme
+https://github.com/fakeuser/Dummy Readme
+# Description
+Testing if code can generate a Readme file
+# Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+# Installation
+The following dependencies must be installed to run the application properly: test
+# Usage
+In order to use this app, Test
+# License
+This project is licensed under the none license. 
+![GitHub license](https://img.shields.io/badge/license-none-blue.svg)
+# Contributing
+​Contributors: Me
+# Tests
+The following is needed to run the test: Test
+# Questions
+If you have any questions about the repo, open an issue or contact fakeuser directly at : fake@email.com.
--- util/generateMarkdown.js
+++ util/generateMarkdown.js
@@ -1,20 +1,31 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+    return `
+# ${data.Title}
+https://github.com/${data.Username}/${data.Title}
+# Description
+${data.Description}
+# Table of Contents 
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+# Installation
+The following dependencies must be installed to run the application properly: ${data.Installation}
+# Usage
+In order to use this app, ${data.Usage}
+# License
+This project is licensed under the ${data.License} license. 
+![GitHub license](https://img.shields.io/badge/license-${data.License}-blue.svg)
+# Contributing
+​Contributors: ${data.Contributing}
+# Tests
+The following is needed to run the test: ${data.Tests}
+# Questions
+If you have any questions about the repo, open an issue or contact ${data.Username} directly at : ${data.Email}.
+`
 }
 
 module.exports = generateMarkdown;
